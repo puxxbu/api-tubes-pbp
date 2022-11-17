99380f8c9eb0e1749ca338281498789f77d3b7a0
puxxbu <puxxbu@users.noreply.github.com>
2022-11-17 09:40:19 +0700

    add api bookmark

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('bookmarks', BookmarkController::class);
